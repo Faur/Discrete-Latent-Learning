dcdb309a9fab7d621ca8fbbc7529ec44f6e63cb2
Toke Faurby <tokeee@gmail.com>
2018-11-05 15:49:03 +0100

    Small changes in test_atari

--- test_atari.py
+++ test_atari.py
@@ -1,20 +1,27 @@
 import gym
 
-env = gym.make("Breakout-v0")
-
 def reset_env(env):
     obs = env.reset()
     reward_sum = 0
     done = False
     return obs, reward_sum, done
 
+env = gym.make("Breakout-v0")
 obs, reward_sum, done = reset_env(env)
+steps = 0
 while(True):
     obs, reward, done, info = env.step(env.action_space.sample())
+    steps += 1
     reward_sum += reward
     env.render()
 
+    if steps >= 512:
+        done = True
+
     if done:
-        print('Episode reward', reward_sum)
+        print('Episode reward: {:5}'.format(reward_sum), end='. ')
+        print('Num steps: {:5}'.format(steps), end='. ')
+        print()
         obs, reward_sum, done = reset_env(env)
+        steps = 0
 
