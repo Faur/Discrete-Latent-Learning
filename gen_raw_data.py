71aca1409b16de53b33475546dfd52244db5879a
Toke Faurby <tokeee@gmail.com>
2018-11-09 11:52:37 +0100

    Generate raw data - split into 'batches'

--- gen_raw_data.py
+++ gen_raw_data.py
@@ -6,14 +6,16 @@ import gym.spaces #TODO: remove this (only used to suppress warning
 import gym_utils
 import data_utils
 
+
 def generate_action(env):
     a = env.action_space.sample()
     return a
 
-def gen_data(gen_args, save=True, render=False):
+
+def gen_data(gen_args, render=False):
     """ Format: (obs, action, reward, done)
     """
-    thread_num, max_steps = gen_args
+    batch_num, postfix, max_steps = gen_args
 
     env = gym.make("Breakout-v0")
     obs_data = []
@@ -35,49 +37,69 @@ def gen_data(gen_args, save=True, render=False):
         else:
             obs = data_utils.normalize_observation(obs_)
 
+    # data_as_array = np.concatenate(obs_data, 0)
+    data_as_array = np.vstack(obs_data)
+
+    ### Compute memory usage of obs
+    # size_of_data = data_utils.getSize_lol(obs_data)
+    # actual_total_obs = len(obs_data[0]) * len(obs_data)
+    # size_per_obs = int(size_of_data/actual_total_obs)
+    # print(data_utils.sizeof_fmt(size_per_obs))  # 24.6KiB
+    # print(size_per_obs)  # 25218 Bytes
+
+    file_name = 'Breakout_raw_{}_{:04d}'.format(postfix, batch_num)
+    data_utils.save_np_array_as_h5(file_name, data_as_array)
+    # print('Generated dataset with ', data_as_array.shape[0], "observations.")
+    # print("Format: (obs, action, reward, done)")
+    print('Saved batch: {:4}'.format(batch_num), '-', file_name)
+
     env.close()
-    return obs_data
+    # return obs_data
+    return file_name
+
 
 def generate_raw_data(total_frames, postfix=''):
     total_frames = int(total_frames)
 
-    num_threads = mp.cpu_count()-1 # TODO: This causes memory issues!
-    num_threads = 1
-    frames_per_thread = int(total_frames/num_threads) + 1
-    gen_args = [(i, frames_per_thread) for i in range(num_threads)]
+    # 256*32*obs_mem_size ~ 0.75 GB
+    max_eps_len = 256  # doesn't actually matter!
+    max_frames_per_thread = max_eps_len*32
+    num_batches = total_frames // max_frames_per_thread
+    frames_in_last_batch = total_frames - max_frames_per_thread * (total_frames // max_frames_per_thread)
+    batch_len = [max_frames_per_thread]*num_batches + [frames_in_last_batch]
+    if frames_in_last_batch != 0:
+        num_batches += 1
+    gen_args = [(i, postfix, batch_len[i]) for i in range(num_batches)]
 
     print("Generating", postfix, "data for env CarRacing-v0")
     print("total_frames: ", total_frames)
-    print("Threads:", num_threads, '(',frames_per_thread,'per thread)')
+    print('max_frames_per_thread', max_frames_per_thread)
+    print("num_batches:", num_batches, '(',batch_len,')')
     print('...')
 
-    with mp.Pool(mp.cpu_count()) as p:
-        data = p.map(gen_data, gen_args)
+    with mp.Pool(mp.cpu_count()-1) as p:
+        # data = p.map(gen_data, gen_args)
+        file_names = p.map(gen_data, gen_args)
 
-    data_as_array = np.concatenate(data, 0)[:total_frames]
+    return file_names
 
-    print('Generated dataset with ', data_as_array.shape[0], "observations.")
-    print("Format: (obs, action, reward, done)")
-    file_name = 'Breakout_raw_'+str(data_as_array.shape[0])+'_'+postfix
-    data_utils.save_np_array_as_h5(file_name, data_as_array)
-    print()
-
-    return file_name
 
 if __name__ == '__main__':
     # TODO: This causes memory issues!
-    train_frames = 1e4
-    # train_frames = 10
-    file_name = generate_raw_data(train_frames, 'train')
+    train_frames = 1e6
+    # # train_frames = 1e4
+    file_names = generate_raw_data(train_frames, 'train')
 
-    # valid_frames = 1e3
-    # file_name = generate_raw_data(valid_frames, 'valid')
+    valid_frames = 1e5
+    file_names = generate_raw_data(valid_frames, 'valid')
 
     if 1:
         print("Load test - Begin.")
+        file_name = file_names[0]
         data_path = './data/' + file_name + '.h5'
         print(data_path)
         data = data_utils.lad_h5_as_np_array(data_path)
+        print('data', type(data))
         print("Load test - Success!")
 
 
